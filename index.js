95a752534d1f7c3c4a5c7598e0f00566efbff508
abdus251 <abdus251@gmail.com>
2024-07-10 05:57:00 +0600

    MyCart backend complited

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 const port = process.env.PORT || 5000;
 
@@ -9,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.u8om2pp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,10 +27,54 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const usersCollection = client.db("bistroDb").collection("users");
     const menuCollection = client.db("bistroDb").collection("menu");
     const reviewCollection = client.db("bistroDb").collection("reviews");
     const cartCollection = client.db("bistroDb").collection("carts");
 
+    app.post('/jwt', (res, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_SECRET_TOKEN, { expiresIn: '1h' })
+
+      res.send({ token })
+    })
+
+
+
+//  users related apis
+app.get('/users', async(req, res) =>{
+  const result = await usersCollection.find().toArray();
+  res.send(result);
+})
+
+app.post('/users', async(req, res) =>{
+  const user = req.body;
+  const query = {email: user.email}
+  const existingUser = await usersCollection.findOne(query);
+  if(existingUser){
+    return res.send({ message: 'user already exist'})
+  }
+  const result = await usersCollection.insertOne(user);
+  res.send(result);
+})
+
+
+   
+app.patch('/users/admin/:id', async(req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const filter = { _id: new ObjectId(id) };
+  const updateDoc = {
+    $set: {
+      role: `admin`
+    },
+  };
+
+  const result = await usersCollection.updateOne(filter, updateDoc);
+  res.send(result);
+})
+
+// menu related apis
     app.get('/menu', async(req, res) =>{
         const result = await menuCollection.find().toArray();
         res.send(result);
@@ -53,11 +98,16 @@ app.get('/carts', async(req, res) => {
   const result = await cartCollection.find(query).toArray();
   res.send(result);
 });
-
 app.post('/carts', async(req, res) => {
   const item =req.body;
-  console.log(item);
   const result = await cartCollection.insertOne(item);
+  res.send(result);
+})
+
+app.delete('/carts/:id', async(req, res) =>{
+  const id = req.params.id;
+  const query = { _id: new ObjectId(id) };
+  const result = await cartCollection.deleteOne(query);
   res.send(result);
 })
 
